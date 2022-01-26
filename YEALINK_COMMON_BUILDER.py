a296934ed0db71f676de8f818c87c07c6ca43894
Jeffrey Mcintyre <redeyecoding@gmail.com>
2022-01-26 13:17:56 -0500

    Add files via upload

---
+++ YEALINK_COMMON_BUILDER.py
@@ -0,0 +1,52 @@
+# %T40P_FIRMWARE%
+# VOC-DEFAULT-YEALINK-COMMON-TEMPLATE
+
+## Grab The Common Files
+common_file_config_path = 'C:\\Users\\jeffrey\\Documents\\YEALINK_UPDATED_TEMPS-2021\\YEALINK_CONFIG_UPDATER\\VOC-DEFAULT-YEALINK-COMMON-TEMPLATE\\'
+
+with open(f'{common_file_config_path}VOC-DEFAULT-YEALINK-COMMON-TEMPLATE.cfg','r') as yealink_configs:
+    yealink_common_template  = yealink_configs.readlines()
+
+
+
+def common_config_builder(year=2021, common_path='C:\\Users\\jeffrey\\Documents\\YEALINK_UPDATED_TEMPS-2021\\YEALINK_CONFIG_UPDATER\\YEALINK-COMMON-BUILDS\\'):
+    yealink_model_list = [
+        'VOC-YEALINK-CP960', 'VOC-YEALINK-T21P',
+        'VOC-YEALINK-T23G',  'VOC-YEALINK-T40G', 
+        'VOC-YEALINK-T40P',  'VOC-YEALINK-T41P',  
+        'VOC-YEALINK-T41S',  'VOC-YEALINK-T42G', 
+        'VOC-YEALINK-T42S',  'VOC-YEALINK-T43U',  
+        'VOC-YEALINK-T46G',  'VOC-YEALINK-CP920',
+        'VOC-YEALINK-T46S',  'VOC-YEALINK-T48G', 
+        'VOC-YEALINK-T48S',  'VOC-YEALINK-T48U',  
+        'VOC-YEALINK-T54W',  'VOC-YEALINK-T57W', 
+        'VOC-YEALINK-T58A',  'VOC-YEALINK-CP930', 
+        'VOC-YEALINK-T46U',  'VOC-YEALINK-T53W','VOC-YEALINK-W60P'
+    ]
+
+   
+    for yealink_model_number in yealink_model_list:
+        #Strip down the model-number
+        model_number = yealink_common_template[1].replace(yealink_common_template[1], f'#{yealink_model_number}'.strip('VOC-')+'\n')
+        base_model_number = model_number.replace('#VOC-YEALINK-', '').strip()
+
+        #Set up naming convention for output file
+        file_name_convention = model_number.replace('#VOC-', '').strip('\n')
+        current_firmware_url = yealink_common_template[310]
+
+        #Setup Firmware URL
+        new_firmware_url = yealink_common_template[310].replace(f'{current_firmware_url}',f'static.firmware.url = tftp://%PROV_SERVER%/Yealink/%{base_model_number}_FIRMWARE%')
+        
+        #Update config with new values
+        yealink_common_template[1] = model_number
+        yealink_common_template[310] = new_firmware_url
+        
+        #Create Common.cfg template for model
+        with open(f'{common_path}{file_name_convention}-COMMON-TEMPLATE-{year}.cfg','a') as yealink_config_file:
+            for line in yealink_common_template:
+                yealink_config_file.write(line)
+    return {
+        'common_file_path': common_path
+    }
+
+common_config_builder()
---
+++ YEALINK_TEMPATE_BUILDER.py
@@ -0,0 +1,112 @@
+from io import BufferedWriter
+import os
+import yealink_common_builder
+
+## Grab The Common Files
+common_file_config_path = 'C:\\Users\\jeffrey\\Documents\\YEALINK_UPDATED_TEMPS-2021\\YEALINK_CONFIG_UPDATER\\YEALINK-COMMON-BUILDS\\'
+completed_configs_directory = 'C:\\Users\\jeffrey\\Documents\\YEALINK_UPDATED_TEMPS-2021\\YEALINK_CONFIG_UPDATER\\YEALINK_CONFIGS\\COMPLETED_YEALINK_CONFIGS\\'
+
+with open('VOC-DEFAULT-YEALINK-MAC-TEMPLATE.cfg','r') as yealink_configs:
+    configs_list  = yealink_configs.readlines()
+
+
+def mac_config_builder(line_number, buffer = '', new_line = '', variable_line = ''):
+    #Create Accounts for mac
+    for line in configs_list:
+        # Replace BroadSoft Variable line
+        if f'-2%' and 'account.line_number' in line:
+            #Replace "account" with new number
+            variable_line = line.replace(f'-2%',f'-{line_number}%')
+            variable_line_2 = variable_line.replace(f'account.line_number',f'account.{line_number}')
+            buffer = buffer + variable_line_2
+
+        elif f'-2%' and 'voice_mail.number.line_number' in line: 
+            #Replace "account" with new number
+            variable_line = line.replace(f'-2%',f'-{line_number}%')
+            variable_line_2 = variable_line.replace(f'voice_mail.number.line_number',f'voice_mail.number.{line_number}')
+            buffer = buffer + variable_line_2
+
+        elif f'-2%' in line:     
+            #Replace "account" with new number
+            variable_line = line.replace(f'-2%',f'-{line_number}%')
+            buffer = buffer + variable_line 
+
+        elif f'account.line_number' in line:
+            #Replace "account" with new number
+            new_line = line.replace(f'account.line_number',f'account.{line_number}')
+            buffer = buffer + new_line
+
+        else:            
+            buffer = buffer + line
+
+    line_number += 1
+    return [line_number, buffer]
+
+
+
+def looper(line_supported, yealink_line_number = 1, buffer=''):    
+    if yealink_line_number > line_supported:
+        # All Accounts were added
+        return [True, buffer]
+
+    yealink_results = mac_config_builder(yealink_line_number, buffer)
+    buffer = yealink_results[1]
+    yealink_line_number = yealink_results[0] 
+
+    return looper(line_supported, yealink_line_number, buffer)
+
+
+def build_yealink_config():
+    #YEALINK SUPPORTED NUMBER OF LINES
+    YEALINK_2 = 2
+    YEALINK_3 = 3
+    YEALINK_6 = 6
+    YEALINK_12 = 12
+    YEALINK_16 = 16
+
+
+    suported_yealink_lines = {
+        
+        'VOC-YEALINK-CP960': YEALINK_2, 'VOC-YEALINK-T21P': YEALINK_2,
+        'VOC-YEALINK-T23G': YEALINK_3, 'VOC-YEALINK-T40G': YEALINK_3, 
+        'VOC-YEALINK-T40P': YEALINK_3, 'VOC-YEALINK-T41P': YEALINK_6 , 
+        'VOC-YEALINK-T41S': YEALINK_6, 'VOC-YEALINK-T42G': YEALINK_12,
+        'VOC-YEALINK-T42S': YEALINK_12, 'VOC-YEALINK-T43U': YEALINK_12, 
+        'VOC-YEALINK-T46G': YEALINK_16, 'VOC-YEALINK-CP920': YEALINK_2,         
+        'VOC-YEALINK-T46S': YEALINK_16, 'VOC-YEALINK-T48G': YEALINK_16,
+        'VOC-YEALINK-T48S': YEALINK_16, 'VOC-YEALINK-T48U': YEALINK_16, 
+        'VOC-YEALINK-T54W': YEALINK_16, 'VOC-YEALINK-T57W': YEALINK_16,
+        'VOC-YEALINK-T58A': YEALINK_16, 'VOC-YEALINK-CP930': YEALINK_2, 
+        'VOC-YEALINK-T46U': YEALINK_16, 'VOC-YEALINK-T53W': YEALINK_12,
+        'VOC-YEALINK-W60P': YEALINK_6
+
+    }
+ 
+
+
+    # Get list of yealink configs
+    list_of_yealink_configs = os.listdir(common_file_config_path)
+
+    #Find model number in config
+    for common_conf_file in list_of_yealink_configs:
+
+        # Open the yealink COMMON config for reading
+        with open(common_file_config_path + common_conf_file,'r') as yealink_configs:
+            yealink_common_file  = yealink_configs.readlines()
+            
+        for line in yealink_common_file:
+            if 'VOC-YEALINK-' in line:
+                stripped_model_number = line.strip()
+                model_number = stripped_model_number.replace('#','')
+                additional_yealink_accounts = looper(suported_yealink_lines[model_number])
+
+               # new_yealink_file = common_conf_file.replace('.cfg','TEST.cfg')
+                with open(completed_configs_directory + model_number+'-TEMPLATE-'+'2021','a') as new_yealink_config_file:
+                    for line in yealink_common_file:
+                        new_yealink_config_file.write(line)
+                    new_yealink_config_file.write(additional_yealink_accounts[1])
+                continue 
+    return
+
+
+build_yealink_config()
